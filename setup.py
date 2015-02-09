94bdadfb5e66d5249919108b099098fa98dc565a
Alexandre Conde <alexandreconde@hotmail.com>
2015-02-09 12:07:29 +0000

    add 'cli' module

--- setup.py
+++ setup.py
@@ -29,8 +29,8 @@ setup(
     url='https://github.com/xlcnd/datafolder',
     packages=['datafolder'],
     entry_points={
-        'console_scripts': ['datafolder_mktpl=datafolder._resources:mktpl',
-                            'datafolder_mkboot=datafolder._resources:mkboot']},
+        'console_scripts': ['datafolder_mktpl=datafolder._cli:mktpl',
+                            'datafolder_mkboot=datafolder._cli:mkboot']},
     license='LGPL v3',
     description='Install and access data files (conf, json, sqlite3, ...) in an easy way.',
     keywords='packaging, setuptools, installation, discover, data files',

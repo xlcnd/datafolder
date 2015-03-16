efc733e1e0715a3e8710204a4d7f1a1c7ebecd1f
Alexandre Conde <alexandreconde@hotmail.com>
2015-03-16 09:52:16 +0000

    release 0.3.6

--- datafolder/_resources.py
+++ datafolder/_resources.py
@@ -74,7 +74,7 @@ setup(
 
 # but we are NOT READY, in some cases the data files
 # don't have the appropriate permissions and 'pip'
-# overwrites all data files that have been 
+# overwrites all data files that have been
 # previously installed (even if they have been changed!).
 # By default '.conf', '.cfg', '.ini' and '.yaml' files
 # are protected, you can change this by passing
@@ -152,7 +152,7 @@ setup(
 
 # but we are NOT READY, in some cases the data files
 # don't have the appropriate permissions and 'pip'
-# overwrites all data files that have been 
+# overwrites all data files that have been
 # previously installed (even if they have been changed!).
 # By default '.conf', '.cfg', '.ini' and '.yaml' files
 # are protected, you can change this by passing
